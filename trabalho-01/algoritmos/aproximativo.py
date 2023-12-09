f332eed87b8d7fcb5a21f7f1ce024f2fe4c9e5ca
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2023-12-09 16:01:26 -0300

    muito update

--- trabalho-01/algoritmos/aproximativo.py
+++ trabalho-01/algoritmos/aproximativo.py
@@ -1,8 +1,178 @@
-# Importing necessary libraries
 import numpy as np
 import os
 import sys
 import time
+
+def tsp(graph_distances):
+    # build a graph
+    G = build_graph(graph_distances)
+
+    # build a minimum spanning tree
+    MSTree = minimum_spanning_tree(G)
+    
+    # find odd vertexes
+    odd_vertexes = find_odd_vertexes(MSTree)
+
+    # add minimum weight matching edges to MST
+    minimum_weight_matching(MSTree, G, odd_vertexes)
+
+    # find an eulerian tour
+    eulerian_tour = find_eulerian_tour(MSTree, G)
+
+    current = eulerian_tour[0]
+    path = [current]
+    visited = [False] * len(eulerian_tour)
+    visited[eulerian_tour[0]] = True
+    length = 0
+
+    for v in eulerian_tour:
+        if not visited[v]:
+            path.append(v)
+            visited[v] = True
+
+            length += G[current][v]
+            current = v
+
+    length += G[current][eulerian_tour[0]]
+    path.append(eulerian_tour[0])
+
+    return path, length
+
+def build_graph(graph_distances):
+    graph = {}
+    for i in range(len(graph_distances)):
+        for j in range(len(graph_distances[i])):
+            if i != j:
+                if i not in graph:
+                    graph[i] = {}
+
+                graph[i][j] = graph_distances[i][j]
+
+    return graph
+
+class UnionFind:
+    def __init__(self):
+        self.weights = {}
+        self.parents = {}
+
+    def __getitem__(self, object):
+        if object not in self.parents:
+            self.parents[object] = object
+            self.weights[object] = 1
+            return object
+
+        # find path of objects leading to the root
+        path = [object]
+        root = self.parents[object]
+        while root != path[-1]:
+            path.append(root)
+            root = self.parents[root]
+
+        # compress the path and return
+        for ancestor in path:
+            self.parents[ancestor] = root
+        return root
+
+    def __iter__(self):
+        return iter(self.parents)
+
+    def union(self, *objects):
+        roots = [self[x] for x in objects]
+        heaviest = max([(self.weights[r], r) for r in roots])[1]
+        for r in roots:
+            if r != heaviest:
+                self.weights[heaviest] += self.weights[r]
+                self.parents[r] = heaviest
+
+def minimum_spanning_tree(G):
+    tree = []
+    subtrees = UnionFind()
+    for W, u, v in sorted((G[u][v], u, v) for u in G for v in G[u]):
+        if subtrees[u] != subtrees[v]:
+            tree.append((u, v, W))
+            subtrees.union(u, v)
+
+    return tree
+
+def find_odd_vertexes(MST):
+    tmp_g = {}
+    vertexes = []
+    for edge in MST:
+        if edge[0] not in tmp_g:
+            tmp_g[edge[0]] = 0
+
+        if edge[1] not in tmp_g:
+            tmp_g[edge[1]] = 0
+
+        tmp_g[edge[0]] += 1
+        tmp_g[edge[1]] += 1
+
+    for vertex in tmp_g:
+        if tmp_g[vertex] % 2 == 1:
+            vertexes.append(vertex)
+
+    return vertexes
+
+def minimum_weight_matching(MST, G, odd_vert):
+    while odd_vert:
+        v = odd_vert.pop()
+        length = float("inf")
+        u = 1
+        closest = 0
+        for u in odd_vert:
+            if v != u and G[v][u] < length:
+                length = G[v][u]
+                closest = u
+
+        MST.append((v, closest, length))
+        odd_vert.remove(closest)
+
+def find_eulerian_tour(MatchedMSTree, G):
+    # find neigbours
+    neighbours = {}
+    for edge in MatchedMSTree:
+        if edge[0] not in neighbours:
+            neighbours[edge[0]] = []
+
+        if edge[1] not in neighbours:
+            neighbours[edge[1]] = []
+
+        neighbours[edge[0]].append(edge[1])
+        neighbours[edge[1]].append(edge[0])
+
+    # finds the hamiltonian circuit
+    start_vertex = MatchedMSTree[0][0]
+    EP = [neighbours[start_vertex][0]]
+
+    while len(MatchedMSTree) > 0:
+        i = 0
+        v = 0
+        for i, v in enumerate(EP):
+            if len(neighbours[v]) > 0:
+                break
+
+        while len(neighbours[v]) > 0:
+            w = neighbours[v][0]
+
+            remove_edge_from_matchedMST(MatchedMSTree, v, w)
+
+            del neighbours[v][(neighbours[v].index(w))]
+            del neighbours[w][(neighbours[w].index(v))]
+
+            i += 1
+            EP.insert(i, w)
+
+            v = w
+
+    return EP
+
+def remove_edge_from_matchedMST(MatchedMST, v1, v2):
+
+    for i, item in enumerate(MatchedMST):
+        if (item[0] == v2 and item[1] == v1) or (item[0] == v1 and item[1] == v2):
+            del MatchedMST[i]
+
+    return MatchedMST
 
 # Function to read a matrix from a file
 def read_matrix_from_file(file_path):
@@ -11,13 +181,6 @@ def read_matrix_from_file(file_path):
         # Converting lines from the file to a 2D array (matrix)
         matrix = [list(map(int, line.split())) for line in lines]
     return np.array(matrix)
-
-
-
-
-
-
-
 
 # Function to let the user choose a TSP file
 def choose_tsp_file():
@@ -40,7 +203,7 @@ def choose_tsp_file():
         sys.exit()
 
 # Number of iterations
-num_iterations = 10
+num_iterations = 50
 
 total_execution_time = 0
 output_file_path2 = 0
@@ -66,14 +229,18 @@ for iteration in range(num_iterations):
     # Criando o caminho absoluto para o arquivo de texto
     file_path = os.path.join(absolute_path, file_name)
 
+    # Lendo a matriz do arquivo escolhido
+    matrix = read_matrix_from_file(file_path)
+    
     # Gravando o tempo de início para medição do tempo de execução
     start_time = time.time()
 
-    # Lendo a matriz do arquivo escolhido
-    matrix = read_matrix_from_file(file_path)
-
-    # Encontrando a solução do vizinho mais próximo para o TSP
-    # result_path, result_cost = nearest_neighbor(matrix)
+    # Get TSP tour and tour length
+    # =======================================
+    
+    tsp_tour, tour_length = tsp(matrix)
+    
+    # =======================================
 
     # Gravando o tempo de término para medição do tempo de execução
     end_time = time.time()
@@ -83,7 +250,7 @@ for iteration in range(num_iterations):
 
     # Imprimindo os resultados
     print(f"Iteração {iteration + 1} - {file_name}:")
-    print("Custo Mínimo:", result_cost)
+    print("Custo Mínimo:", tour_length)
     print("Tempo de Execução:", execution_time, "segundos")
     print("-" * 30)
 
@@ -95,7 +262,7 @@ for iteration in range(num_iterations):
     output_file_path2 = output_file_path
     with open(output_file_path, 'a') as output_file:
         output_file.write(f"Iteração {iteration + 1}:\n")
-        output_file.write(f"Custo Mínimo: {result_cost}\n")
+        output_file.write(f"Custo Mínimo: {tour_length}\n")
         output_file.write(f"Tempo de Execução: {execution_time} segundos\n")
         output_file.write("-" * 30 + "\n")
 
